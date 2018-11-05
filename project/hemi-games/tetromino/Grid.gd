fcce9d5a09c6a64881a09c5fc631e7bf92a3b030
Jason Yundt <swagfortress@gmail.com>
2018-11-05 07:20:31 -0500

    Added grid lines

--- project/hemi-games/tetromino/Grid.gd
+++ project/hemi-games/tetromino/Grid.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-const WIDTH = 10
-const HEIGHT = 20
+const WIDTH = 10 # in squares
+const HEIGHT = 20 # in squares
 
 # NOTE: It is assumed that SQUARES is a horizontal sprite sheet with 8 sprites
 export(Texture) var SQUARES = null
@@ -20,7 +20,13 @@ func _ready():
 	# squares[r][c]
 	# r is for rows starting at the bottom (I would start at the top, but that would result in row 0 being in the buffer zone)
 	# c is for columns starting on the left side
-	for r in range(HEIGHT*2): # add extra space over the top to make room for off screen locks (make sure the border covers it)
+	var SQUARE_WIDTH = SQUARES.get_height()
+	var SQUARE_HEIGHT = SQUARES.get_width() / 8
+	var HEIGHT_IN_PIXELS = SQUARE_HEIGHT * HEIGHT # height of the entire grid
+	
+	$Lines.position.y -= SQUARE_HEIGHT
+	
+	for r in range(HEIGHT): # add extra space over the top to make room for off screen locks (make sure the border covers it)
 		squares.append([])
 		for c in range(WIDTH):
 			var to_add = Sprite.new()
@@ -29,13 +35,9 @@ func _ready():
 			to_add.hframes = 8
 			
 			# Make sure the sprite is in the right position
-			var SQUARE_WIDTH = to_add.texture.get_height()
-			var SQUARE_HEIGHT = to_add.texture.get_width() / 8
-			var SCREEN_HEIGHT = get_viewport_rect().size.y
-			
 			to_add.centered = false
 			var x = c * SQUARE_WIDTH
-			var y = SCREEN_HEIGHT - (r+1) * SQUARE_HEIGHT
+			var y = HEIGHT_IN_PIXELS - (r+1) * SQUARE_HEIGHT
 			to_add.position = Vector2(x, y)
 			
 			to_add.hide()
